50770b70054e968d24e10404913b0427ef4e468c
Pritesh Vardekar <priteshv.264@gmail.com>
2023-11-18 13:23:49 +0530

    completed first version

--- word-counter.js
+++ word-counter.js
@@ -1,2 +1,19 @@
-let box = document.getElementById("box");
-let count = document.getElementById("count");
+let textarea = document.getElementById("textarea");
+let char = document.getElementById("char");
+let word = document.getElementById("word");
+
+textarea.addEventListener("input", function () {
+    var text = this.value;
+    // for char count
+    let charlength = text.length;
+    char.innerHTML = charlength;
+
+    // for words count
+    //trim() removes the extra spaces
+    let wordsinbox = text.trim().split(" ");
+    let cleanlist = wordsinbox.filter(function (elm) {
+        return elm != "";
+    });
+    word.innerHTML = cleanlist.length;
+
+});
